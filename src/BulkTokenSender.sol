976277583da45107a87760863a716e8358cafd1b
pmoncada <pmoncada@umich.edu>
2024-11-08 15:58:37 -0800

    some sketches for the deprize contract

--- src/BulkTokenSender.sol
+++ src/BulkTokenSender.sol
@@ -22,4 +22,90 @@ contract BulkTokenSender {
             require(tokenContract.transferFrom(msg.sender, recipients[i], amounts[i]), "Token transfer failed");
         }
     }
+
+
+    function uploadAllocationResult(address[] memory recipients, uint256[] memory percents) public {
+        assert(recipients.length == percents.length);
+    
+        // get current timestamp
+        uint256 currentTimestamp = block.timestamp;
+        assert(currentTimestamp > lastAllocationUpdateTimestamp);
+
+        // find the difference between the current timestamp and the last allocation update timestamp
+        uint256 timeSinceLastUpdate = currentTimestamp - lastAllocationUpdateTimestamp;
+        lastAllocationUpdateTimestamp = currentTimestamp;
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            if (i >= currentRecipients.length) {
+                //append the new recipient
+                currentRecipients.push(recipients[i]);
+                currentAmounts.push(0);
+                currentSplitPercent.push(percents[i]);
+            } else {
+                assert(currentRecipients[i] == recipients[i]);
+                currentAmounts[i] += timeSinceLastUpdate * totalPrizeAllocationPerSecond * currentSplitPercent[i];
+            }
+
+            
+            // update the current split percent
+            currentSplitPercent[i] = percents[i];
+        }
+    }
+
+
+    function claimAllocation(address token) public {
+        // get current timestamp
+        uint256 currentTimestamp = block.timestamp;
+        // find the difference between the current timestamp and the last allocation update timestamp
+        uint256 timeSinceLastUpdate = currentTimestamp - lastAllocationUpdateTimestamp;
+                lastAllocationUpdateTimestamp = currentTimestamp;
+
+        for (uint256 i = 0; i < currentRecipients.length; i++) {
+            currentAmounts[i] += timeSinceLastUpdate * totalPrizeAllocationPerSecond * currentSplitPercent[i];
+
+            if (currentRecipients[i] == msg.sender) {
+                uint256 amountToWithdraw = currentAmounts[i];
+                currentAmounts[i] = 0;
+                IERC20Interface tokenContract = IERC20Interface(token);  // Initialize the token contract interface
+                require(tokenContract.transfer(msg.sender, amountToWithdraw), "Token transfer failed");
+            }
+        }
+
+
+    }
+
+
+
+    function addPrize(address token, uint256 amount) public {
+        IERC20Interface tokenContract = IERC20Interface(token);  // Initialize the token contract interface
+        // transfer the amount from the user to the contract
+        require(tokenContract.transferFrom(msg.sender, address(this), amount), "Token transfer failed");
+
+        // calculate the total prize allocation per second
+        totalPrize = totalPrize + amount;
+
+        // Convert to fixed point math to handle decimals
+        totalPrizeAllocationPerSecond = (totalPrize * 10 / 100) / 30 days;
+    }
+
+
+    // Total amount of $PRIZE
+    uint256 public totalPrize;
+
+    // Total amount of $PRIZE allocated per second
+    uint256 public totalPrizeAllocationPerSecond;
+
+    // Last timestamp when allocations were updated
+    uint256 public lastAllocationUpdateTimestamp;
+
+
+    // Current recipients of the allocation
+    address[] public currentRecipients;
+
+    // Current split percent of the allocation
+    uint256[] public currentSplitPercent;
+
+    // Current amounts of the allocation
+    uint256[] public currentAmounts;
+
 }
